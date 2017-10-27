2a1ccab264496d3666091b1f051410e434ec62cd
Felix Grund <felix.grund@scandio.de>
2017-10-26 17:04:01 -0700

    now has a squirrel image

--- node_app/flyweb_server/flyweb_server.js
+++ node_app/flyweb_server/flyweb_server.js
@@ -1,12 +1,11 @@
 (function() {
 
-	let indexHtml = `
+	var indexHtml = `
 <!DOCTYPE html>
 <html>
 <head>
     <title>Queuera</title>
     <link rel="stylesheet" type="text/css" href="css/queuera.css">
-    <script src="js/handlebars.js"></script>
     <script src="js/jquery.js"></script>
     <script src="js/wsapi.js"></script>
 </head>
@@ -14,15 +13,31 @@
 <body>
 
     <div id="main">
-    	<img src="jpg/biergarten.jpg">
-	</div>
+        <div>
+            <span>Hello </span><span id="name"></span><span>!</span>
+        </div>
+        <div>
+            <span>Queue: </span><span id="queue"></span>
+        </div>
+        <div>
+            <span>Student: </span>
+            <a id="add" href="#">Add me!</a>
+            <a id="remove" href="#">Remove me!</a>
+        </div>
+        <div>
+            <span>TA: </span>
+            <a id="pop" href="#">Pop</a>
+        </div>
+        
+        <img src="jpg/squirrel.jpg">
+    </div>
 
     <script src="js/queuera.js"></script>
 </body>
 </html>
 `;
 
-	let mimeTypes = {
+	var mimeTypes = {
 		css: "text/css",
 		js: "application/javascript",
 		jpg: "image/jpeg"
@@ -36,17 +51,17 @@
 
 	navigator.publishServer("FlyWeb Queuera").then(function(server) {
 		server.onfetch = function(event) {
-			let url = event.request.url;
+			var url = event.request.url;
 			if (url !== "/") {
-				let filename = url.split("/").pop();
-				let fileExtension = filename.split(".").pop();
-				let mimeType = mimeTypes[fileExtension];
-				let options = createOptions(mimeType);
+				var filename = url.split("/").pop();
+				var fileExtension = filename.split(".").pop();
+				var mimeType = mimeTypes[fileExtension];
+				var options = createOptions(mimeType);
 				fetch(url)
 					.then(response => response.blob())
 					.then(blob => event.respondWith(new Response(blob, createOptions(mimeType))));
 			} else {
-				let options = createOptions("text/html");
+				var options = createOptions("text/html");
 				event.respondWith(new Response(indexHtml, options));
 			}
 
